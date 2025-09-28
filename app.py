93b26aa135e9da07ec70ff8198eb29c5eb162d93
vuzv <kyrylkravchenko1994@gmail.com>
2025-09-28 19:19:36 +0200

    refactoring

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from gtts import gTTS
 from langdetect import detect, detect_langs
 from llm import request
 from prompts import raw_dev_prompt, raw_user_prompt
+import re
 
 load_dotenv()
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
@@ -25,25 +26,42 @@ def match_target_language(source_lang):
             return 'en'
 
 
+def extract_prefix_symbols(text):
+    match = re.match(r'^([^a-zA-Z0-9]*)', text)
+    if match:
+        symbols = match.group(1)
+        remaining_text = text[len(symbols):].strip()
+        return (symbols, remaining_text)
+    return ('', text)
+
+
+def parse_symbols(commands, text):
+    language = detect(text)
+    if '@' in commands:
+        language = 'de'
+    action = os.getenv('ACTION_TRANSLATE')
+    if '!' in commands:
+        action = os.getenv('ACTION_CORRECT')
+    if '?' in commands:
+        action = os.getenv('ACTION_EXPLAIN')
+    return {'language': language, 'action': action, 'tts': '*' in commands}
+
+
 @bot.message_handler()
 def command_help(message):
-    language = detect(message.text)
-    text = message.text
-    action = os.getenv('ACTION_TRANSLATE')
-    if text[0] == '!':
-        action = os.getenv('ACTION_CORRECT')
-        text = text[1:].strip()
-    elif text[0] == '?':
-        action = os.getenv('ACTION_EXPLAIN')
-        text = text[1:].strip()
-    target_language = match_target_language(language)
-    dev_prompt = raw_dev_prompt.format(from_lang=language, to_lang=target_language)
-    user_prompt = raw_user_prompt.format(action=action, text=text)
-    response = request(dev_prompt, user_prompt)
-    if text[0] == '*':
-        tts = gTTS(text=response, lang=detect(response), slow=False)
+    bot.send_chat_action(message.chat.id, 'typing')
+    commands, text = extract_prefix_symbols(message.text)
+    parsed = parse_symbols(commands, text)
+    if parsed['tts']:
+        bot.send_chat_action(message.chat.id, 'record_audio')
+        tts = gTTS(text=text, lang=parsed['language'], slow=False)
         tts.save(mp3_path)
         bot.send_voice(message.chat.id, open(mp3_path, 'rb'))
+    target_language = match_target_language(text)
+    dev_prompt = raw_dev_prompt.format(from_lang=parsed['language'], to_lang=target_language)
+    user_prompt = raw_user_prompt.format(action=parsed['action'], from_lang=parsed['language'], text=text)
+    bot.send_chat_action(message.chat.id, 'typing')
+    response = request(dev_prompt, user_prompt)
     bot.reply_to(message, response)
 
 
--- prompts.py
+++ prompts.py
@@ -1,9 +1,8 @@
 raw_dev_prompt = """
-You are a professional deep {from_lang} to {to_lang} language translator, tutor, and corrector.
-Detect the language of the input if not specified.
+You are a professional deep \'{from_lang}\' to \'{to_lang}\' language translator, tutor, and corrector.
 If you are prompted to translate, just provide the translation without any additional commentary.
 """
 
 raw_user_prompt = """
-{action} this text: {text}
+{action} this \'{from_lang}\' text: {text}
 """
